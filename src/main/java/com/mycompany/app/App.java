c4336ddd966bc795d8e8f2a7419e3d45419bc2e5
yalgun <yavuzalgun34@gmail.com>
2019-02-02 19:07:47 +0300

    Projeye son sekli verildi

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -32,21 +32,34 @@ public class App
           //System.out.println(req.queryParams("input2"));
 
           String input1 = req.queryParams("input1");
+          String input3 = req.queryParams("input3");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
+          java.util.Scanner sc2 = new java.util.Scanner(input3);
           sc1.useDelimiter("[;\r\n]+");
           java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
             int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
             inputList.add(value);
           }
+		  while (sc2.hasNext())
+          {
+            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            inputList2.add(value);
+          }
           System.out.println(inputList);
+          System.out.println(inputList2);
 
 
           String input2 = req.queryParams("input2").replaceAll("\\s","");
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
           int input2AsInt = Integer.parseInt(input2);
+          int input4AsInt = Integer.parseInt(input4);
 
           boolean result = App.search(inputList, input2AsInt);
+		  boolean result2 = App.search(inputList2, input4AsInt);
+		  result = result || result2; 
 
          Map map = new HashMap();
           map.put("result", result);
